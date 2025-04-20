401e035986ed175b8b8b220a4f94c6ce90f5b8c5
AryaPatel2110 <patelarya899@gmail.com>
2025-04-20 10:52:40 -0500

    Filter User Data

--- notebooks/utils/config.py
+++ notebooks/utils/config.py
@@ -2,14 +2,12 @@
 from pathlib import Path
 
 # S3 paths or local paths for data files
-BUSINESS_DATA_PATH = "../datasets/yelp_academic_dataset_business.json"
-REVIEW_DATA_PATH = "../datasets/yelp_academic_dataset_review.json"
-OUTPUT_BUSINESS_PATH = ""
-OUTPUT_REVIEW_PATH = ""
-YELP_RESTAURANT = ""
+BUSINESS_DATA_PATH = "../sourcedata/yelp_academic_dataset_business.json"
+REVIEW_DATA_PATH = "../sourcedata/yelp_academic_dataset_review.json"
+USER_DATA_PATH = "../sourcedata/yelp_academic_dataset_user.json"
 
 PHILADELPHIA = "../datasets/philadelphia.json"
-USER = "../datasets/yelp_academic_dataset_user.json"
+USER = "../datasets/user.json"
 
 # Spark
 APP = "C:\spark"
